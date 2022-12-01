aa0030ef40bf1cde489686718c4fb050351db070
allan <allankretzmann@hotmail.com>
2022-12-01 18:45:06 -0300

    Deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://programacao-web-projeto3-backend.vercel.app/",
 });
 
 export default api;
